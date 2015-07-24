e77eb556ed0ff2bf5c5e77609d897e8e6df872b2
Andy Mauragis <andrew@mauragis.com>
2015-07-24 11:15:13 -0400

    Initial Commit

---
+++ BofhDotNet/ConsoleUtilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BofhDotNet
+{
+    // Utilities for working with operating system console interface.
+    public static class ConsoleUtilities
+    {
+        public static void WriteError(string message, params string[] args)
+        {
+            UseTextColor(ConsoleColor.Red, () => Console.Error.WriteLine(message, args));
+        }
+
+        public static void WriteWarning(string message, params string[] args)
+        {
+            UseTextColor(ConsoleColor.Yellow, () => Console.Error.WriteLine(message, args))
+        }
+
+        public static void UseTextColor(ConsoleColor colour, Action action)
+        {
+            var prevForegroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = colour;
+            action();
+            Console.ForegroundColor = prevForegroundColor;
+        }
+    }
+}
